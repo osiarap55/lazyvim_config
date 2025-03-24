bf7638ecf1ea6c01de5fea18dbc40ea08b7e3a56
Armando Garia <islandrainning@gmail.com>
2025-03-24 16:18:18 +0000

    actualizacion

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,12 +1,24 @@
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
+--
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme catppuccin-mocha")
+    end,
   },
 }
